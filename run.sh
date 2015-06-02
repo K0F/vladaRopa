78277a2995355c1b868840ad448e20163e7add1c
k0f <krystof.pesek@gmail.com>
2015-06-03 01:05:58 +0200

    better scripts

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin/
 
 sudo touch /forcefsck
 
-(mkfifo /tmp/ctl)&
+#(mkfifo /tmp/ctl)&
 
-(sleep 15s && sh /home/pi/rewind.sh)&
+(sleep 5s && sh /home/pi/sensor.sh)&
 
-while true; do
-(sleep 4s && omxplayer -b -o local --no-osd /home/pi/vlada_vystava_FINAL.mov < /tmp/ctl)
-done
+#while true; do
+#(sleep 4s && omxplayer -b -o local --no-osd /home/pi/vlada_vystava_FINAL.mov < /tmp/ctl)
+#done
--- sensor.sh
+++ sensor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#(sh /home/pi/rewind.sh && sleep 2s && sh /home/pi/play.sh && sleep 7m && sleep 30s && sh /home/pi/play.sh)
 #
+
 gpio mode 11 in
 
 while true
@@ -9,7 +11,8 @@ if [ `gpio read 11` -eq 1 ];
 then
 
 echo 'Motion Detected: '`date +"%Y-%m-%d_%H-%M-%S"`
-(sh /home/pi/rewind.sh && sleep 2s && sh /home/pi/play.sh && sleep 7m && sleep 30s && sh /home/pi/play.sh)
+(omxplayer -b -o local --no-osd /home/pi/vlada_vystava_FINAL.mov && sleep 7m && sleep 33s) 
+
 
 else
 
