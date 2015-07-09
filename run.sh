0c0a493cba45843fad7c8aa5c702694dfa1026d6
k0f <krystof.pesek@gmail.com>
2015-07-09 02:29:34 +0200

    sync tag 1.0

--- run.sh
+++ run.sh
@@ -4,10 +4,20 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin/
 
 sudo touch /forcefsck
 
+(sleep 15s && LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libv4l/v4l2convert.so motion > /dev/null 2>&1)&
+
+
+
 #(mkfifo /tmp/ctl)&
 
-(sleep 5s && sh /home/pi/sensor.sh)&
+#(sleep 5s && sh /home/pi/sensor.sh)&
 
-#while true; do
-#(sleep 4s && omxplayer -b -o local --no-osd /home/pi/vlada_vystava_FINAL.mov < /tmp/ctl)
-#done
+while true; do
+if [ -e /home/pi/ok ]
+then
+pkill omxplayer
+sudo rm /home/pi/ok
+omxplayer -b -o local --no-osd /home/pi/vlada_vystava_FINAL.mov > /dev/null 2>&1
+fi
+sleep 2s
+done
